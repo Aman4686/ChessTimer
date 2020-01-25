26fb71488348aa76fafafc9d8be7be3c2d4377f4
Aman4686 <MrAman@ukr.net>
2020-01-25 11:58:37 +0300

    add button click

--- app/src/main/java/com/example/chesstimer/timer/TimerView.kt
+++ app/src/main/java/com/example/chesstimer/timer/TimerView.kt
@@ -1,9 +1,13 @@
 package com.example.chesstimer.timer
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
 import com.example.chesstimer.Base.BaseView
@@ -12,14 +16,43 @@ import com.example.chesstimer.databinding.TimerMainBinding
 
 class TimerView(@NonNull inflater: LayoutInflater, @NonNull lifecycleOwner: LifecycleOwner,
                  @Nullable container: ViewGroup , @NonNull val model: TimerViewModel) : BaseView() {
+
+    private val context : Context
     init {
         val mDataBinding : TimerMainBinding = DataBindingUtil.inflate(inflater, R.layout.timer_main, container, false)
         mDataBinding.lifecycleOwner = lifecycleOwner
         mDataBinding.executePendingBindings()
         mDataBinding.viewModel = model
+
         viewLayout = mDataBinding.root
+        context = mDataBinding.root.context
+    }
+
+
+    companion object {
+        @BindingAdapter("invertMainBtnColor")
+        @JvmStatic
+        fun LinearLayout.invertMainBtnColor(bol: Boolean) {
+            val context = context
+            background = if (bol) {
+                context.getDrawable(R.color.colorDark)
+            } else {
+                context.getDrawable(R.drawable.timer_primary_bottom_btn)
+            }
+        }
+        @BindingAdapter("invertSecondaryBtnColor")
+        @JvmStatic
+        fun TextView.invertSecondaryBtnColor(bol: Boolean) {
+            val context = context
+            background = if (bol) {
+                context.getDrawable(R.color.colorDark)
+            } else {
+                context.getDrawable(R.drawable.timer_secondary_btn)
+            }
+        }
     }
 }
+
 
 
 
--- app/src/main/java/com/example/chesstimer/timer/TimerViewModel.kt
+++ app/src/main/java/com/example/chesstimer/timer/TimerViewModel.kt
@@ -1,8 +1,25 @@
 package com.example.chesstimer.timer
 
+import android.view.View
+import android.widget.LinearLayout
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.chesstimer.R
 import com.example.chesstimer.common.navigation.TimerNavigator
 
 class TimerViewModel(val navController : TimerNavigator) : ViewModel() {
+    val model = MutableLiveData<Boolean>()
+
+    fun onTopButtonClicked(v : View) {
+        model.value = true
+    }
+
+    fun onBottomButtonClicked(v : View) {
+        model.value = false
+    }
+
+
 
 }
